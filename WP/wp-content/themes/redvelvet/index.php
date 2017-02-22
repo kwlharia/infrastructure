77018800b9e4bdbaeb583df8ad632bc8ff4c7f91
wilburrr90 <wilburrr90@gmail.com>
2017-02-21 20:36:06 -0500

    Add initial codebase

---
+++ WP/wp-content/themes/redvelvet/index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div id='main'>
+  <h1>MAIN AREA</h1>
+  <a href='announcements'>Announcements</a>
+  <a href='assignments'>Assignments</a>
+  <a href='course-material'>Course Material</a>
+  <a href='discussion-board'>Discussion Board</a>
+  <a href='grades'>Grades and Attendance</a>
+  <a href='group-projects'>Group Projects</a>
+</div>
+<div id='delimiter'></div>
+<?php get_footer(); ?>
---
+++ WP/wp-content/themes/redvelvet/templates/group-projects.php
@@ -0,0 +1,5 @@
+<?php /* Template Name: group-projects */ ?>
+
+<div>
+  Here are the group projects
+</div>
